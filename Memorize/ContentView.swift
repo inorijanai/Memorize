3d333b91ce1269c8cd8b5f0be6eecae3a9be60a7
inori <inorijanai@gmail.com>
2023-11-02 18:08:17 +0800

    test/testing push in XCode

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -14,6 +14,7 @@ struct ContentView: View {
             CardView()
             CardView()
             CardView()
+            //changed something
         }
         .foregroundColor(.orange)
         .padding()
