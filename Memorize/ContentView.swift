a1d2f2ebbdd12532a1c9b15ffc3df58bf873c3a2
inori <inorijanai@gmail.com>
2023-11-04 16:21:31 +0800

    Finished Programming Assignment 1

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -8,24 +8,32 @@
 import SwiftUI
 
 struct ContentView: View {
-    let emojis: [String] = [ "😤", "😇", "🤔", "😃", "😃", "😤", "😇", "🤔", "😃", "😃" ]
+    @State var emojis: [String] = []
     
-    @State var cardCount: Int = 4
+    let emotions = ["🥹", "😅", "☺️", "😇", "😫", "😭", "😡", "😢", "🥶", "😱"]
+    let animals = ["🐶", "🐱", "🐭", "🐰", "🦊", "🐻", "🐮", "🐷"]
+    let weathers = ["☀️", "🌤️", "☁️", "🌧️", "⛈️", "💦", "☔️", "⛄️", "🌈"]
     
     var body: some View {
         VStack{
+            title
             ScrollView{
                 cards
             }
             Spacer()
-            cardCountAdjusters
+            themeSwichers
         }
         .padding()
     }
     
+    var title: some View{
+        Text("Memorize!")
+            .font(.largeTitle)
+    }
+    
     var cards: some View{
-        LazyVGrid(columns: [GridItem(), GridItem(), GridItem()]) {
-            ForEach(0..<cardCount, id: \.self) { index in
+        LazyVGrid(columns: [GridItem(), GridItem(), GridItem(), GridItem(), GridItem()]) {
+            ForEach(0..<emojis.count, id: \.self) { index in
                 CardView(content: emojis[index])
                     .aspectRatio(2/3, contentMode: .fit)
             }
@@ -33,43 +41,73 @@ struct ContentView: View {
         .foregroundColor(.orange)
     }
     
-    var cardCountAdjusters: some View {
-        HStack{
-            cardRemover
-            Spacer()
-            cardAdder
-        }
-        .imageScale(.large)
-        .font(.largeTitle)
-    }
+//    var cardCountAdjusters: some View {
+//        HStack{
+//            cardRemover
+//            cardAdder
+//        }
+//        .imageScale(.large)
+//        .font(.largeTitle)
+//    }
     
-    func cardAdjuster(by offset: Int, symbol: String) -> some View {
+    func themeSelector(by theme: String, symbol: String, lebal: String) -> some View{
         Button(action: {
-            cardCount += offset
+            switch theme {
+            case "emotion":
+                emojis = (emotions + emotions).shuffled()
+            case "animal":
+                emojis = (animals + animals).shuffled()
+            case "weather":
+                emojis = (weathers + weathers).shuffled()
+            default:
+                print("No such theme")
+            }
         }, label: {
-            Image(systemName: symbol)
+            VStack{
+                Image(systemName: symbol).font(.title).imageScale(.large)
+                Text(lebal)
+            }
         })
-        .disabled(cardCount + offset < 1 || cardCount + offset > emojis.count)
     }
     
-    var cardRemover: some View {
-        cardAdjuster(by: -1, symbol: "rectangle.stack.badge.minus.fill")
+    var themeSwichers: some View {
+        HStack{
+            Group{
+                themeSelector(by: "emotion", symbol: "face.smiling", lebal: "Emotions")
+                themeSelector(by: "animal", symbol: "globe.asia.australia", lebal: "Animals")
+                themeSelector(by: "weather", symbol: "sun.max", lebal: "Weathers")
+            }.padding([.top, .leading, .trailing])
+        }
+//        .font(.largeTitle)
     }
     
-    var cardAdder: some View {
-        cardAdjuster(by: +1, symbol: "rectangle.stack.badge.plus.fill")
-    }
+//    func cardAdjuster(by offset: Int, symbol: String) -> some View {
+//        Button(action: {
+//            
+//        }, label: {
+//            Image(systemName: symbol)
+//        })
+////        .disabled(cardCount + offset < 1 || cardCount + offset > emojis.count)
+//    }
+    
+//    var cardRemover: some View {
+//        cardAdjuster(by: -1, symbol: "rectangle.stack.badge.minus.fill")
+//    }
+//    
+//    var cardAdder: some View {
+//        cardAdjuster(by: +1, symbol: "rectangle.stack.badge.plus.fill")
+//    }
 }
 
 struct CardView: View {
     let content: String
     // Views are immutable but their body can be changed
     // "@State" is just for small things
-    @State var isFaceUp = true
+    @State var isFaceUp = false
     
     var body: some View {
         ZStack() {
-            let base = RoundedRectangle(cornerRadius: 25.0)
+            let base = RoundedRectangle(cornerRadius: 12)
             // apply .opacity() to all views in Group
             Group {
                 base.fill(.white)
